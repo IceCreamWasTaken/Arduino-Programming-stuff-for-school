d69d0346c6f885faf2641e1e0ef500672b655072
IceCreamWasTaken <albertisarcher@gmail.com>
2022-05-12 12:59:34 +1000

    v.01 on lightsensor
    frnoeaijrghgieorshiohierso

---
+++ sketch_may02a/lightsensor.ino
@@ -0,0 +1,14 @@
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    int analogValue = analogRead(A1);
+
+    Serial.print("Brightness: ")
+    Serial.print(analogValue);
+
+    if (analogValue <10) {
+        Serial.println("- Dark");
+    })
+}
