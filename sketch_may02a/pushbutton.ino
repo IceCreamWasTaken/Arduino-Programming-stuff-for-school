0d3c98a62e2601739181e886f28c8727d3995c9e
IceCreamWasTaken <albertisarcher@gmail.com>
2022-05-12 12:43:55 +1000

    added pushbutton code v.1

--- sketch_may02a/pushbutton.ino
+++ sketch_may02a/pushbutton.ino
@@ -1,1 +1,25 @@
-const int buttonPin = 
+const int buttonPin = 8;
+const int LED = 6;
+int buttonState = 0;
+int flag = 0;
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(butttonPin, INPUT_PULLUP); 
+}
+
+void loop() {
+    buttonState = digitalRead(buttonPin)
+    
+    if (buttonState == LOW) {
+        if (flag == 0){
+            digitalWrite(LED, HIGH);
+            flag = 1;
+        }
+    else 
+    if (flag == 1){
+        digitalWrite(LED, LOW);
+        flag = 0;
+    }
+}
+}
