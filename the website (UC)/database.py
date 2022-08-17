3ba8e398111c31a3fa16fb3f8079145f2c479cd4
IceCreamWasTaken <88661398+IceCreamWasTaken@users.noreply.github.com>
2022-08-17 17:06:48 +1000

    Delete database.py

--- the website (UC)/database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-print("Opened database successfully!")
-
-conn.execute("CREATE TABLE TTSRecords (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, audio TEXT)")
-print("Table created successfully")
-conn.close()
