4e2778c216117559e12f3247cbd04ddcd4af95c6
IceCreamWasTaken <albertisarcher@gmail.com>
2022-08-31 18:37:35 +1000

    code comments on app and main

--- the better website (UC)/app.py
+++ the better website (UC)/app.py
@@ -1,21 +1,28 @@
 # Imports libraries neccessary for the website to operate with flask
-from flask_cors import cross_origin
+from flask_cors import cross_origin # exposes all routes, origins and methods or Cross Origin Resource Sharing (CORS)
 from flask import Flask, render_template, request
-from main import text_to_speech
+from main import text_to_speech # pulls a definition from main.py to be able to run TTS on a web server
 
-app = Flask(__name__)
-from app import app
+app = Flask(__name__) # Just a passing argument of the Flask class. Basically it's a filler that gets replaced of 
+# "the name of the application package".
+# You pass the function as the __name__ is varied depending on what you call the .py file name (e.g. main.py and app.py)
+from app import app 
+# Quickstart. First app is the name of the package and the second app is the imported object from the same package
 
-@app.route('/', methods=['POST', 'GET'])
-@cross_origin()
-def thewebsite():
-    if request.method == 'POST':
-        text = request.form['speech']
-        gender = request.form['voices']
-        text_to_speech(text, gender)
-        return render_template('frontend.html')
+@app.route('/', methods=['POST', 'GET']) # maps the specific URL managing the logic for that URL. 
+# POST and GET are both python requests usually when HTML interacts with python implementation. GET is request data while
+# POST is submit data to be processed to the server in which is what Flask sets up.
+@cross_origin() # resource sharing
+def thewebsite(): # creates a definition 
+    if request.method == 'POST': # If the button was clicked, sends a POST request
+        text = request.form['speech'] # Turns the text into a file that can be readable via a TTS API
+        gender = request.form['voices'] # Based off from the user input, the voice will be based off from what gender was
+        # selected
+        text_to_speech(text, gender) # message and who spoke it
+        return render_template('frontend.html') # generates an output (the text that gets played)  
     else:
-        return render_template('frontend.html')
+        return render_template('frontend.html') # generates an output (the text that gets played)
 
+# Flask stuff, sets up the server and connect it to a port and generates a link (a localhost) to view
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- the better website (UC)/main.py
+++ the better website (UC)/main.py
@@ -1,33 +1,41 @@
 import pyttsx3
+# imports a text-to-speech library in Python. There are several APIs that are able to take user generated text
+# into an audio file that is playable such as pyttsx3 (the one that's being used), google text-to-speech (able to
+# translate a variety of languages, also known as gTTS) and Amazon Polly all of which are three different APIs 
+# however, they do the same function. The reason why I chose pyttsx is because of real world applications being able to 
+# work offline and is compatiable for those who still use python 2.
 
-def text_to_speech(text, gender):
+def text_to_speech(text, gender): # Creates a definition of what's to be included in the program. 
     """
     Function to convert text to speech
     :param text: text
     :param gender: gender
     :return: None
     """
-    voice_dict = {'Male': 0, 'Female': 1}
-    code = voice_dict[gender]
+    voice_dict = {'Male': 0, 'Female': 1} # What voices will be available to use.
+    code = voice_dict[gender] # Defines the number is based on the gender of what the TTS program will read on.
 
-    engine = pyttsx3.init()
+    engine = pyttsx3.init() # Sets the engine of the TTS API. Initialises it to flask server. 
 
-    # Setting up voice rate
+    # Setting up voice rate. How many words will the API speaks per minute. The higher the number, the faster it
+    # gets. The lower the number, the slower it gets. 
     engine.setProperty('rate', 125)
 
-    # Setting volume level between 0 the minimum an 1 the max
-    engine.setProperty('volume', 0.85)
+    # Setting volume level between 0 the minimum an 1 the max. Higher the decimal, the louder it is. 
+    engine.setProperty('volume', 0.85) # Fixed volume level (stays as 0.85 until changed via the code itself)
 
-    # Change voices: 0 for male and 1 for female (there's only gender voices)
+    # Change voices: 0 for male and 1 for female (there's only gender voices), basically pulls 
+    # the voices needed in TTS library to use for the website.
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[code].id)
 
-    engine.say(text)
+    # When setting up flask these two commands will run to test if it works. These will run in command prompt/terminal
+    engine.say(text) 
     engine.runAndWait()
 
-text = "Hello! This is a test of text to speech!"
+text = "Hello! This is a test of text to speech! Thanks." # the text that's being played when running flask server
 gender = 'Male' # The voice assistant, remember 0 for male, 1 for female drawn from the import
-text_to_speech(text, gender)
+text_to_speech(text, gender) # runs a script from a definition from above. 
 
 
 
